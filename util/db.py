676feee4ec49edea0da59289733f334bc4c21044
MuspiMerol <wjj123345567@iCloud.com>
2022-04-04 04:00:21 +0800

    second commit

--- util/db.py
+++ util/db.py
@@ -1,9 +1,18 @@
 from ..secret import host, port, db, user, password, dialct, driver
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-uri = f"{dialct}+{driver}://{user}:{password}@{host}:{port}/{db}"
+url = f"{dialct}+{driver}://{user}:{password}@{host}:{port}/{db}"
+
+Base = declarative_base()
+engine = create_engine(url)
+Session = sessionmaker(engine)
 
 
+def create_all():
+    return Base.metadata.create_all(engine)
+
+
+session = Session()
