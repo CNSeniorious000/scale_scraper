a255a3814ba89e00aa8568620d825b578da47228
MuspiMerol <wjj123345567@iCloud.com>
2022-04-13 04:10:57 +0800

    make the whole project object-originated

--- util/request.py
+++ util/request.py
@@ -1,40 +1,73 @@
-import requests
+from functools import cached_property, cache
 from bs4 import BeautifulSoup
-
-# noinspection HttpUrlsUsage
-base = "http://www.obhrm.net"
-
-r = requests.get(f"{base}/index.php/栏目:研究量表")
-bs = BeautifulSoup(r.text)
-
-# start from 4
-groups = bs.html.body.find_all("div")[2].find_all("div")[4].find_all("div")[3].find_all("div")[4:]
+import requests
 
 
-def parse_group(group):
-    return [li.a["href"] for li in group.find_all("li")]
+class OBHRM:
+    # noinspection HttpUrlsUsage
+    base = "http://www.obhrm.net"
+    __current_page = None
+
+    @classmethod
+    @cache
+    def get(cls, url: str) -> requests.Response:
+        url.removesuffix(cls.base)
+        return requests.get(f"{cls.base}{url}")
+
+    @property
+    def current_page(self):
+        return self.__current_page or self.root_page
+
+    @cached_property
+    def root_page(self) -> BeautifulSoup:
+        return BeautifulSoup(self.get("/index.php/栏目:研究量表").text, "lxml")
+
+    @classmethod
+    def turn_to_next_page(cls):
+        cls.__current_page = ...
+
+    @cached_property
+    def groups(self) -> list[BeautifulSoup]:
+        return self.current_page.find_all("div")[2].find_all("div")[4].find_all("div")[3].find_all("div")[4:]
+
+    @staticmethod
+    @cache
+    def get_paths(group: BeautifulSoup) -> list[str]:
+        return [li.a["href"] for li in group.find_all("li")]
+
+    @cache
+    def parse_page(self, path: str) -> tuple[str, list[tuple[str, list[str]]]]:
+        soup = BeautifulSoup(self.get(path).text, "lxml")
+        # body = soup.body.find_all("div")[2].find_all("div")[3].find_all("div")[3]
+        title = soup.head.title.text.removesuffix(" - OBHRM百科")
+        toc: BeautifulSoup = soup.body.find_all("div")[2].find_all("div")[3].find_all("div")[3].div
+
+        main_body = []
+        for tag in toc.find_next_siblings():
+            if tag.name == "p":
+                main_body[-1][1].append(tag.text.strip())
+                if tag.a is not None:
+                    main_body[-1][1].append(self.base + self.get_resource_url(tag.a["href"]))
+
+            elif tag.name == "h2":
+                new_subtitle = tag.text
+                main_body.append((new_subtitle, []))
+            elif tag.name == "pre":
+                main_body[-1][1].append(tag.text.strip())
+            else:
+                raise ValueError(tag)
+
+        return title, main_body
+
+    @classmethod
+    @cache
+    def get_resource_url(cls, url: str) -> str:
+        return BeautifulSoup(cls.get(url).text, "lxml").select_one(".internal")["href"]
 
 
-# noinspection PyShadowingNames
-def parse_page(path):
-    page = BeautifulSoup(requests.get(base + path).text)
-    title = page.head.title.text.removesuffix(" - OBHRM百科")
-    body = page.body.find_all("div")[2].find_all("div")[3].find_all("div")[3]
+obhrm = OBHRM()
 
-    toc = body.div
-
-    results = []
-    for tag in toc.find_next_siblings():
-        if tag.name == "h2":
-            new_title = tag.text
-            results.append((new_title, []))
-        elif tag.name == "p":
-            results[-1][1].append(tag)
-        else:
-            print(tag)
-            results[-1][1].append(tag)
-
-    return title, results
-
-
-title, results = parse_page(parse_group(groups[2])[2])
+if __name__ == '__main__':
+    _, results = obhrm.parse_page(obhrm.get_paths(obhrm.groups[4])[6])
+    print(results[2][1][-1])
+    print(results[2][1][-2])
